06d858c32945ff37542bc7a2316632fc642e162b
ilya <ilya@2cit.ru>
2019-09-03 16:38:00 +0300

    add tasks and test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_installed_binary(host):
+    f = host.file('/usr/local/sbin/wal-g')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.mode == 0o755
